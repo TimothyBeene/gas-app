cfe3ea2d5fa08c080f1a0f3ea6eddbbfe5f5a6d0
Timothy Beene <tibeene@gmail.com>
2018-03-18 17:06:23 -0500

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  googleClientId: '594820868776-bgt38hgh612g6sis74q2cg9d801dtd6h.apps.googleusercontent.com',
+  googleClientId: '594820868776-75oeoic5ion7f772gnlrvbog7a363skp.apps.googleusercontent.com',
   sheetId: '1HpD_49Y2d2vRfMRmXk8JrNX2LtUfNCIArdZO9fKXHLI',
   googleSheetsBaseUrl: 'https://sheets.googleapis.com/v4/spreadsheets'
 };
