56a56654936c25dc7ea24324c0322fb0600295a1
Timothy Beene <tibeene@gmail.com>
2018-03-18 20:58:54 -0500

    Remove test code from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { SheetsService } from './sheets.service';
 import { UserService } from './auth/user.service';
 import { Router } from '@angular/router';
-import { GasDataService } from './gas-data.service';
 import { Component, OnInit } from '@angular/core';
 import * as ClientOAuth2 from 'client-oauth2';
-import { GoogleApiService, GoogleAuthService } from 'ng-gapi';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,14 +15,13 @@ export class AppComponent implements OnInit {
   gasData;
 
   constructor(
-    private gasDataService: GasDataService,
     private router: Router,
-    private gapiService: GoogleApiService,
     private userService: UserService,
     private sheetsService: SheetsService
   ) {}
 
   ngOnInit() { }
+
   onSubmit() {
     console.log('submit');
     this.sheetsService.pushGasRecord({
@@ -39,12 +34,6 @@ export class AppComponent implements OnInit {
       this.pricePaid = null;
     });
 
-  }
-  test() {
-    this.sheetsService.pushGasRecord({value: 'one'});
-    this.sheetsService.getDataFromSheet('A1:C99')
-        .map(res => res['valueRanges'][0]['values'])
-        .subscribe(res => console.log(res));
   }
   onAuthGoogle() {
     this.userService.signIn();
