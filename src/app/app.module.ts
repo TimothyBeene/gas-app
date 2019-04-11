d3cca625964f3440568ba7e3d51f53cdb25ea245
Timothy Beene <tibeene@gmail.com>
2019-04-10 23:58:41 -0500

    fix pwa config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -69,7 +69,7 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     ...MatModules,
     RouterModule.forRoot(appRoutes, { useHash: true }),
-    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     GoogleApiModule.forRoot({
       provide: NG_GAPI_CONFIG,
       useValue: environment.gapiClientConfig
